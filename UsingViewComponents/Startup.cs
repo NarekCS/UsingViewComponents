5e7bd8d2fc7041810c298de7c58ca159c1643168
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-07 18:41:50 +0400

    Configuring application

--- UsingViewComponents/Startup.cs
+++ UsingViewComponents/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using UsingViewComponents.Models;
 
 namespace UsingViewComponents
 {
@@ -15,20 +16,18 @@ namespace UsingViewComponents
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IProductRepository, MemoryProductRepository>();
+            services.AddSingleton<ICityRepository, MemoryCityRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
