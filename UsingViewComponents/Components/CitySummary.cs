8533c13b207d2d74b4355c9d8235115032c1c565
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-10 16:47:49 +0400

    Hybrid controller-viewComponent

---
+++ UsingViewComponents/Components/CitySummary.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private ICityRepository repository;
+        public CitySummary(ICityRepository repo)
+        {
+            repository = repo;
+        }
+        //public IViewComponentResult Invoke()
+        //{
+        //    //string target = RouteData.Values["id"] as string;
+        //    //var cities = repository.Cities.Where(city => target == null || string.Compare(city.Country, target, true) == 0);
+        //    //return View(new CityViewModel
+        //    //{
+        //    //    Cities = cities.Count(),
+        //    //    Population = cities.Sum(c => c.Population)
+        //    //});
+        //    //return View(new CityViewModel
+        //    //{
+        //    //    Cities = repository.Cities.Count(),
+        //    //    Population = repository.Cities.Sum(c => c.Population)
+        //    //});
+        //    // return Content("This is a <h3><i>string</i></h3>");
+        //    //return new HtmlContentViewComponentResult(new HtmlString("This is a <h3><i>string</i></h3>"));
+        //}
+        public IViewComponentResult Invoke(bool showList)
+        {
+            if (showList)
+            {
+                return View("CityList", repository.Cities);
+            }
+            else
+            {
+                return View(new CityViewModel
+                {
+                    Cities = repository.Cities.Count(),
+                    Population = repository.Cities.Sum(c => c.Population)
+                });
+            }
+        }
+    }
+}
